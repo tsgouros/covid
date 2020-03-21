beff1dfd48c701e7b0fc638a7d4da8ba00fa17a7
tsgouros <tsgouros@gmail.com>
2020-03-21 18:09:01 -0400

    squeeze out zeros, add state image

--- ridges.r
+++ ridges.r
@@ -24,6 +24,41 @@ fullData <- fullData[fullData$location %in% countries,];
 fullData$time <- as.numeric(anytime(fullData$date));
 fullData$time <- (fullData$time - min(fullData$time)) / (24 * 60 * 60)
 
+## Squeeze out the momentary zeros.
+squeeze <- function(data) {
+    keep <- c();
+    for (loc in levels(data$location)) {
+        subData <- data[data$location == loc,];
+        subKeep <- rep(TRUE, length(subData$logCases));
+        if (length(subKeep) >= 3) {
+            for (i in 2:(length(subKeep)-1)) {
+                if ((subData$logCases[i] == 0) &&
+                    ((subData$logCases[i-1] > 0) &&
+                     (subData$logCases[i+1] > 0))) {
+                    subKeep[i] <- FALSE;
+                }
+                if (i < (length(subKeep)-1)) {
+                    if ((subData$logCases[i] == 0) &&
+                        ((subData$logCases[i-1] > 0) &&
+                         (subData$logCases[i+2] > 0))) {
+                        subKeep[i] <- FALSE;
+                    }
+                }
+            }
+
+            if ((subData$logCases[length(subData$logCases)] == 0) &&
+                (subData$logCases[length(subData$logCases) - 1] > 0)) {
+                subKeep[length(subData$logCases)] <- FALSE;
+            }
+        }
+
+        keep <- c(keep, subKeep);
+    }
+    return(data[keep,]);
+}
+
+fullData <- squeeze(fullData);
+fullData <- squeeze(fullData);
 
 gridge <- ggplot(fullData,
                  mapping=aes(x=time, y=location,
@@ -37,4 +72,27 @@ gridge <- ggplot(fullData,
     theme_ridges();
 
 ggsave("images/ridgeplot.png", plot=gridge, device="png");
+
+
+stateData <- read.csv("data/daily.csv", header=TRUE, stringsAsFactors=TRUE);
+
+stateData$date <- as.numeric(anytime(as.character(stateData$date)));
+stateData$date <- (stateData$date - min(stateData$date)) / (24 * 60 * 60)
+
+stateData <- stateData[with(stateData,
+                            order(state, date)),];
+
+stateRidge <- ggplot(stateData,
+                     mapping=aes(x=date, y=state, fill=state,
+                                 height=positive, scale=1.5)) +
+    geom_density_ridges_gradient(stat="identity", show.legend=FALSE) +
+    scale_y_discrete(expand = c(0.01,0)) +
+    scale_x_continuous(expand = c(0.01, 0)) +
+    labs(x="days since 3/4/20", y="total confirmed cases") +
+    coord_cartesian(clip = "off") +
+    theme_ridges();
+
+ggsave("images/stateridges.png", plot=stateRidge, device="png");
+
+
 
